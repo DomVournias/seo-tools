abd877025626dd0eaa9e5cf6dbb088b6df0eab20
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2023-02-01 21:36:28 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,15 +10,19 @@ app.use(express.json());
 app.use("/api", require("./routes/PageSEO_router"));
 app.use("/api", require("./routes/Docs_router"));
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "./client/build")));
+// Serve frontend
 
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname, "./", "client", "build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => res.send("Please set to production"));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "client", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
